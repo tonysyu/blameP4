b3d6e078087f2a916f8499bb39bc53955ee2e6ca
Tony S Yu <tsyu80@gmail.com>
2015-12-30 08:35:37 -0600

    Fix bin/main.js on Windows systems

--- bin/main.js
+++ bin/main.js
@@ -4,9 +4,15 @@ var child_process = require('child_process');
 var process = require('process');
 
 var rootDir = __dirname + '/..';
-var electron = rootDir + '/node_modules/.bin/electron'
+var electron = rootDir + '/node_modules/.bin/electron';
 var additionalArgs = process.argv.slice(2).join(' ');
 
+var isWindows = /^win/.test(process.platform);
+if (isWindows) {
+    electron += '.cmd';
+}
+
+console.log("Executing: " + electron + " " + rootDir + " " + additionalArgs);
 var appProcess = child_process.spawn(electron, [rootDir, additionalArgs]);
 
 appProcess.stderr.on('data', function (error) {
